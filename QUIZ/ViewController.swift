1943d7852e34dd6ab6a1f85f23e4dd80c1686af0
Adrian1690 <j.adrian.1690@gmail.com>
2017-02-22 22:24:34 -0500

    set conections

--- QUIZ/ViewController.swift
+++ QUIZ/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is Cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Monpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +37,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+    }
 
+    @IBAction func showAnswer(_ sender: UIButton) {
+    }
 }
 
